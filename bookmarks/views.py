cbaff446607e0c582e6bd9252b08220d7afc1384
Nathan Daniel <natdf@icloud.com>
2018-05-24 01:46:06 -0500

    list and add bookmarks

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Bookmark
 
 # Create your views here.
 
 def index(request):
-    context = {}
+    context = {
+        'bookmarks': Bookmark.objects.all(),
+    }
     return render(request, 'bookmarks/index.html', context)
